4801b125531d6db85cd27bd368beef21d587da93
Esteban <serafa99@hotmail.com>
2022-04-27 16:19:38 -0400

    Terminado

--- static/mail/inbox.js
+++ static/mail/inbox.js
@@ -36,8 +36,20 @@ document.addEventListener('DOMContentLoaded', function() {
   
 });
 
+// Cuando damos click a algun elemento de la bandeja, carga el div "mail-view"
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("bandeja").addEventListener("click", function(e){      
+    if(e.target && e.target.matches("div.div-mailbox")){
+      let texto = e.target.id; 
+      numero = texto.slice(4);  
+      bandeja =  document.querySelector('#titulo').innerText;
+      console.log(bandeja);
+      load_mail(numero, bandeja);
+    };     
+  })
+});
 
-
+// La funcion que nos envia al form para crear emails
 function compose_email() {
 
   // Show compose view and hide other views
@@ -51,8 +63,26 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
-function load_mailbox(mailbox) {
-  
+// Similar a la anterior, pero sirve para responder
+function compose_response(recipients, subject, timestamp, sender, body) {
+
+  // Show compose view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#mail-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'block';
+
+  // Clear out composition fields
+  document.querySelector('#compose-recipients').value = sender;
+  document.querySelector('#compose-subject').value = 'Re: ' + subject;
+  document.querySelector('#compose-body').value = 'En ' + timestamp + ' ' + sender + ' escribio: ' + body ;
+
+   console.log(recipients)
+   console.log(subject)
+}
+
+//La funcion que carga  la bandeja
+function load_mailbox(mailbox) {  
+
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#mail-view').style.display = 'none';
@@ -70,34 +100,93 @@ function load_mailbox(mailbox) {
       document.getElementById("bandeja").innerHTML = "";
       //Imprimimos los emails            
       for (let i = 0; i < emails.length; i++){      
-      let color = "white";
-      if (emails[i].read){ color = "gray"}
-      document.querySelector("#bandeja").insertAdjacentHTML("afterbegin", `<div class="div-mailbox" id="div-${emails[i].id}" style="background-color: ${color}"><b>${emails[i].sender}</b><br>${emails[i].subject}<br>${emails[i].timestamp}</div><br>`);
+        let color = "white";
+        if (emails[i].read){ color = "gray"}
+          document.querySelector("#bandeja").insertAdjacentHTML("afterbegin", `<div class="div-mailbox" id="div-${emails[i].id}" style="background-color: ${color}"><b>${emails[i].sender}</b><br>${emails[i].subject}<br>${emails[i].timestamp}</div><br>`);
       }  
-    });
-  
-    document.getElementById("bandeja").addEventListener("click", function(e){
-      document.querySelector('#emails-view').style.display = 'none';
-      document.querySelector('#mail-view').style.display = 'block';
-      document.querySelector('#compose-view').style.display = 'none';
-
-      if(e.target && e.target.matches("div.div-mailbox")){
-        let texto = e.target.id
-        console.log(texto.slice(4));
-        fetch('/emails/' + texto.slice(4))
-        .then(response => response.json())
-        .then(email=>{
-          console.log(email)          
-          document.getElementById("mail-view").innerHTML = `Mensaje de:  <b>${email.sender}</b><br>Asunto:  ${email.subject}<br><br>${email.body}<br><br>${email.timestamp}`
-        })
-        .then(fetch('/emails/' + texto.slice(4), {
-          method: 'PUT',
-          body: JSON.stringify({
-              read: true
-          })
-        }))       
-      };
-    })    
+    });   
+      
+     
+    return false          
     
-
 }
+
+
+// Cargamos un correo
+function load_mail(mail, bandeja){
+
+  //Cargamos el mail-view y ocultamos las demas
+  document.querySelector('#mail-view').style.display = 'block';  
+  document.querySelector('#emails-view').style.display = 'none';  
+  document.querySelector('#compose-view').style.display = 'none';
+  
+  document.getElementById('vista').style.display = 'block';
+  document.getElementById('botones').style.display = 'block';
+
+  fetch('/emails/' + mail)
+  .then(response => response.json())
+  .then(email=>{                  
+    document.getElementById("vista").innerHTML = `Mensaje de:  <b>${email.sender}</b><br>Asunto:  ${email.subject}<br><br>${email.body}<br><div id="id">${email.id}</div>${email.timestamp}`;         
+    document.getElementById("id").style.visibility = "hidden";
+    document.getElementById("botones").innerHTML ="";
+    document.getElementById("botones").append(responder);
+    if (email.archived && bandeja !== 'Sent'){            
+      document.getElementById("botones").append(desarchivar);
+    }
+    else if (!email.archived && bandeja !== 'Sent'){  
+      document.getElementById("botones").append(archivar);
+    }   
+       
+  })        
+    .then(fetch('/emails/' + mail, {
+      method: 'PUT',
+      body: JSON.stringify({
+      read: true
+      })
+    }))      
+
+    return false
+}
+
+// Otra forma es crear el elemento afuera
+const archivar = document.createElement('button');
+archivar.innerHTML = 'Archivar';
+archivar.addEventListener('click', function(){
+    fetch('/emails/' + document.getElementById("id").innerHTML, {
+    method: 'PUT',
+    body: JSON.stringify({
+    archived: true
+    })
+  });
+  load_mailbox('inbox');    
+  return load_mailbox('inbox'); 
+});
+
+
+const desarchivar = document.createElement('button');
+desarchivar.innerHTML = 'Desarchivar';
+desarchivar.addEventListener('click', function(){  
+  fetch('/emails/' + document.getElementById("id").innerHTML, {
+    method: 'PUT',
+    body: JSON.stringify({
+    archived: false
+    })
+  }); 
+  load_mailbox('inbox');   
+  return load_mailbox('inbox');
+});
+
+const responder = document.createElement('button');
+responder.innerHTML = 'Responder';
+responder.addEventListener('click', function(){
+  console.log('probando responder');
+  console.log(document.getElementById("id").innerHTML);
+  fetch('/emails/' + document.getElementById("id").innerHTML)
+  .then(response => response.json())
+  .then(email=>{    
+    compose_response(email.recipients, email.subject, email.timestamp, email.sender, email.body);
+  });
+  return   
+});
+
+
